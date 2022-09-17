5c6b7ad16a4db12779b645bbaa7a1374558abbaf
unknown <james.tarrobal.777@gmail.com>
2022-09-17 21:27:11 +0800

    Add ASS_Gen

--- src/data/scripts/ASS_ModPlugin.java
+++ src/data/scripts/ASS_ModPlugin.java
@@ -1,9 +1,13 @@
 package data.scripts;
 
 import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+
+import data.scripts.world.ASS_Gen;
 
 public class ASS_ModPlugin extends BaseModPlugin {
     @Override
     public void onNewGame() {
+        new ASS_Gen().generate(Global.getSector());
     }
 }
---
+++ src/data/scripts/world/ASS_Gen.java
@@ -0,0 +1,10 @@
+package data.scripts.world;
+
+import com.fs.starfarer.api.campaign.SectorAPI;
+import com.fs.starfarer.api.campaign.SectorGeneratorPlugin;
+
+public class ASS_Gen implements SectorGeneratorPlugin {
+    @Override
+    public void generate(SectorAPI sector) {
+    }
+}
