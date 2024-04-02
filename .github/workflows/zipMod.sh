82dd5baccbc23055394e93cb40bfcaecc26f8f47
AudaxLudos <james.tarrobal.777@gmail.com>
2024-04-03 00:00:47 +0800

    Update GitHub action scipt

--- .github/workflows/zipMod.sh
+++ .github/workflows/zipMod.sh
@@ -4,9 +4,9 @@ folderToZip="./../.."
 startingDir=$(pwd)
 
 echo "Executing dir: $startingDir"
-echo "Location of folder to zip:" $(realpath $folderToZip)
+echo "Location of folder to zip:" "$(realpath $folderToZip)"
 
-cd $folderToZip
+cd $folderToZip || return
 
 # Get the mod version number and mod name from mod_info.json
 json=$(cat mod_info.json)
@@ -14,7 +14,8 @@ modVersion=$(echo "$json" | jq -r '.version')
 outputFolderName=$(echo "$json" | jq -r '.name')
 
 # Rename the text before modVersion
-zipName="SuitableStarSystems-$modVersion.zip"
+noSpaceName=$(echo "$outputFolderName" | sed 's/ //g')
+zipName="$noSpaceName-$modVersion.zip"
 
 # Recreate the temp folder if it happens to be present.
 rm -rf "./$outputFolderName"
@@ -23,18 +24,18 @@ mkdir "$outputFolderName"
 # 1. List all files in git, which uses gitignore.
 # 2. Remove any file matching the blacklist.
 # 3. Copy to a new folder with the mod name.
-echo "Reading blacklist...\n$(cat $startingDir/blacklist.txt)"
-git ls-files | grep -Evf $startingDir/blacklist.txt | while read file; do cp --parents "$file" "$outputFolderName"; done
+printf "Reading blacklist...\n%s" "$(cat "$startingDir"/blacklist.txt)"
+git ls-files | grep -Evf "$startingDir"/blacklist.txt | while read -r file; do cp --parents "$file" "$outputFolderName"; done
 
 # Zip the folder, then clean it up.
-zip -r $zipName "./$outputFolderName"
-echo "Created zip file at $(realpath $zipName)"
+zip -r "$zipName" "./$outputFolderName"
+echo "Created zip file at $(realpath "$zipName")"
 rm -rf "./$outputFolderName"
 
 # Move the zip to the artifacts folder in the executing directory.
-mkdir -p $startingDir/artifacts
-mv ./$zipName $startingDir/artifacts/
-echo "Moved zip file to $(realpath $startingDir/artifacts/$zipName)"
+mkdir -p "$startingDir"/artifacts
+mv ./"$zipName" "$startingDir"/artifacts/
+echo "Moved zip file to $(realpath "$startingDir"/artifacts/"$zipName")"
 
 # Return to the executing directory.
-cd $startingDir
+cd "$startingDir" || return
