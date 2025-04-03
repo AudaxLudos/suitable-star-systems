8892b790dd8041a57f0d0add86d73855c38afa76
AudaxLudos <james.tarrobal.777@gmail.com>
2025-04-03 14:38:39 +0800

    Remove old compatibility code for redacted planets

--- src/suitablestarsystems/ModPlugin.java
+++ src/suitablestarsystems/ModPlugin.java
@@ -2,17 +2,11 @@ package suitablestarsystems;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.PlanetAPI;
-import com.fs.starfarer.api.campaign.StarSystemAPI;
-import com.fs.starfarer.api.impl.campaign.ids.StarTypes;
-import com.fs.starfarer.api.impl.campaign.ids.Tags;
 import lunalib.lunaSettings.LunaSettings;
 import suitablestarsystems.campaign.InitialSystemAccess;
 import suitablestarsystems.world.systems.System1;
 import suitablestarsystems.world.systems.System2;
 import suitablestarsystems.world.systems.System3;
-
-import java.util.Objects;
 
 public class ModPlugin extends BaseModPlugin {
     @Override
@@ -38,28 +32,6 @@ public class ModPlugin extends BaseModPlugin {
     public void onGameLoad(boolean newGame) {
         Global.getSector().getMemoryWithoutUpdate().set("$sss_omegaPlanetQuestOverride", Utils.OMEGA_PLANET_QUEST_OVERRIDE);
         Global.getSector().addTransientScript(new InitialSystemAccess());
-
-        // mark remnant and omega system spawned by this mod as not random mission target for existing saves
-        for (StarSystemAPI system : Global.getSector().getStarSystems()) {
-            if (system.isProcgen()) {
-                continue;
-            }
-            if (system.getStar() == null) {
-                continue;
-            }
-            if (!Objects.equals(system.getStar().getTypeId(), StarTypes.BLACK_HOLE) || !Objects.equals(system.getStar().getTypeId(), StarTypes.WHITE_DWARF)) {
-                continue;
-            }
-            for (PlanetAPI planet : system.getPlanets()) {
-                if (!Objects.equals(planet.getId(), "planet_sss_remnant") || !Objects.equals(planet.getId(), "planet_sss_omega")) {
-                    continue;
-                }
-                if (planet.hasTag(Tags.NOT_RANDOM_MISSION_TARGET)) {
-                    continue;
-                }
-                planet.addTag(Tags.NOT_RANDOM_MISSION_TARGET);
-            }
-        }
     }
 
     @Override
